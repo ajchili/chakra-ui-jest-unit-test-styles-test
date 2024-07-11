33bf950e8e17df017de882ba7dea70e58e994547
Kirin Patel <kirin.patel@datavant.com>
2024-07-10 22:34:37 -0400

    🔧 document test case failure mode

--- src/components/ShrinkableText/ShrinkableText.spec.tsx
+++ src/components/ShrinkableText/ShrinkableText.spec.tsx
@@ -8,13 +8,18 @@ it("does not shrink when rendered in a parent node with no width bounds", async 
   expect($element).toBeFalsy();
 });
 
-it("shrinks when rendered in a parent node with width bounds", async () => {
-  const { queryByText } = render(<ShrinkableText />, {
+it("fails to shrink when rendered in a parent node with width bounds in a test environment", async () => {
+  const { container, queryByText } = render(<ShrinkableText />, {
     wrapper: (ui) => (
       <div style={{ width: 20, maxWidth: 20 }}>{ui.children}</div>
     ),
   });
 
   const $element = await queryByText("shrunk");
-  expect($element).toBeTruthy();
+  expect($element).toBeFalsy();
+
+  // This fails because JSDOM is strictly a DOM API emulation library, not a document renderer
+  const $textElement = await container.querySelector("p");
+  expect($textElement.clientWidth).toBe(0); // clientWidth is 0 because the thing isn't ever rendered 😛
+  expect($textElement.scrollWidth).toBe(0); // scrollWidth is 0 because the thing isn't ever rendered 😛
 });
