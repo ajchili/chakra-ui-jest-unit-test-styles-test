3f2b72c84ba4953861514df4751d8136e1ca2ed9
Kirin Patel <kirinpatel@gmail.com>
2024-07-10 20:06:14 -0400

    🔧 fix test case

--- src/components/SomeComponent/SomeComponent.spec.tsx
+++ src/components/SomeComponent/SomeComponent.spec.tsx
@@ -1,9 +1,16 @@
 import { render } from "@testing-library/react";
 import { SomeComponent } from "./SomeComponent";
 
-it("works", async () => {
+it("fails to resolve class styles when accessing styles directly", async () => {
   const { findByText } = render(<SomeComponent />);
 
   const $element = await findByText("SOME TEXT");
-  expect($element.style.background).toBe("red");
+  expect($element.style.background).not.toBe("red");
+});
+
+it("resolve class styles when accessing styles through getComputedStyles", async () => {
+  const { findByText } = render(<SomeComponent />);
+
+  const $element = await findByText("SOME TEXT");
+  expect(getComputedStyle($element).background).toBe("red");
 });
--- src/components/SomeComponent/SomeComponent.tsx
+++ src/components/SomeComponent/SomeComponent.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@chakra-ui/react";
 
 export const SomeComponent = () => {
-  return <Text background={"red"}>SOME TEXT</Text>;
+  return <Text background="red">SOME TEXT</Text>;
 };
